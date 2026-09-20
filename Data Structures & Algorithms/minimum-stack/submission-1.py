59a8e4918401a87acd6ab145762acb3d9da0e0d0
nish-codes-25 <vinishaadappa.51.e.fe@gmail.com>
2026-09-20 19:39:36 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        minVal = min(self.minStack[-1], val) if self.minStack else val
+        self.minStack.append(minVal)
+
+    def pop(self) -> None:
+        self.minStack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
